6fea537a04be8d8db6e86bb6e2de0e90a40ed6b6
Wantedkillers <Wantedkillerss@users.noreply.github.com>
2015-04-23 16:59:51 -0400

    unexpected end of file

--- src/Pocketwarriors/colors/Main.php
+++ src/Pocketwarriors/colors/Main.php
@@ -13,7 +13,19 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase implements listener {
+namespace Pocketwarriors\colors;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+
+class Main extends PluginBase{
   
      public function onEnable(){
           $this->getLogger()->info(TextFormat::AQUA." Loaded Successfully!");
@@ -23,69 +35,70 @@ class Main extends PluginBase implements listener {
           $this->getLogger()->info(TextFormat::AQUA." Unloaded Successfully!");
      }
 
-     public function onCommand(CommandSender $sender, Command $cmd, array $args){
+     public function onCommand(CommandSender $issuer, Command $cmd, array $args){
+                   if($issuer->hasPermission("hidetag.command")){
+               if($issuer instanceof Player){
           switch($cmd->getName()){
           case "tag":
           if($sender->hasPermission("tag.command.blue") or $sender->hasPermission("tag.command")){
           switch($args[0]){
                         case "blue":
-	if($sender->hasPermission("tag.command.blue") or $sender->hasPermission("tag.command")){
-              }else{
-	$sender->sendMessage("You do not have permission to use that command.");             
-        return true;
-              }
+          if($sender->hasPermission("tag.command.blue") or $sender->hasPermission("tag.command")){
           $currentTag = $sender->getName();
           $sender->setNameTag("§1$currentTag");
-          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to blue");                                             
+          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to blue");         
+                        return true;
+              }else{
+	$sender->sendMessage("You do not have permission to use that command.");             
+      
+              }
+                                      
                         break;
                         case "red":
-			if($sender->hasPermission("tag.command.red") or $sender->hasPermission("tag.command")){
+     if($sender->hasPermission("tag.command.red") or $sender->hasPermission("tag.command")){
+    $currentTag = $issuer->getName();      
+          $sender->setNameTag("§c$currentTag");
+          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to red");   
+			
                                           }else{
 	$sender->sendMessage("You do not have permission to use that command.");
         return true;
                                           }
-           $currentTag = $sender->getName();
-          $sender->setNameTag("§c$currentTag");
-          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to red");
+
                         
                         break;
                         case "green":
-			if($sender->hasPermission("tag.command.green") or $sender->hasPermission("tag.command")){
+                            if($sender->hasPermission("tag.command.green") or $sender->hasPermission("tag.command")){
+                                    $currentTag = $issuer->getName();
+          $sender->setNameTag("§a$currentTag");
+          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to green");
                                        return true;
                                           }else{
 	$sender->sendMessage("You do not have permission to use that command.");
         return true;
                                           }
-          $currentTag = $sender->getName();
-          $sender->setNameTag("§a$currentTag");
-          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to green");
-     
+  
 			break;
                         case "gold":
-     
-         if($sender->hasPermission("tag.command.gold") or $sender->hasPermission("tag.command")) {
+                if($sender->hasPermission("tag.command.gold") or $sender->hasPermission("tag.command")) {
+               $currentTag = $issuer->getName();
+          $sender->setNameTag("§6$currentTag");
+          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to gold"); 
              return true;       
                                           }else{
 	$sender->sendMessage("You do not have permission to use that command.");
         return true;
                                           }
-          $currentTag = $sender->getName();
-          $sender->setNameTag("§6$currentTag");
-          $sender->sendMessage("[ColorfulTag] Your NameTag has been changed to gold");                                      
+                                     
 			break;
     case "reset":
-			if($sender->hasPermission("tag.command.reset") or $sender->hasPermission("tag.command")){
-                         return true;
+  if($sender->hasPermission("tag.command.reset") or $sender->hasPermission("tag.command")){
+                 $currentTag = $issuer->getName();
+         $sender->setNameTag($sender->getName());
+          $sender->sendMessage("[ColorfulTag] Your NameTag has been reset!");
+                        return true;
+  
                                           }else{
 	$sender->sendMessage("You do not have permission to use that command.");
         return true;
                                           }
-         $currentTag = $sender->getName();
-         $sender->setNameTag($sender->getName());
-          $sender->sendMessage("[ColorfulTag] Your NameTag has been reset!");
-          break;
-     }
-    }
-   }
-  }
- }
